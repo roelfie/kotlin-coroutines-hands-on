82f0f87d23d4e79a75cf6f26b35f0aac59b7ecd0
Roelfie <58694687+roelfie@users.noreply.github.com>
2022-06-03 17:19:45 +0200

    Update UI on each API response. API calls in serial.

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,20 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    var users = emptyList<User>()
+    var ix = 0
+    repos.forEach { repo ->
+        val bodyList: List<User> = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        users = (users + bodyList).aggregate()
+        ix++
+        updateResults(users, ix == repos.size)
+    }
 }
